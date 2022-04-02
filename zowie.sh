82dc53da4017bb6eb75ec85d1dc1e3393af6fa3c
czarhex <76652465+czarhex@users.noreply.github.com>
2022-04-02 10:49:18 +0200

    Delete zowie.sh

--- zowie.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-echo "Fixing acceleration..."
-xinput set-prop 'BenQ ZOWIE BenQ ZOWIE Gaming Mouse' "libinput Accel Profile Enabled" 0, 1
-xinput set-prop 'BenQ ZOWIE BenQ ZOWIE Gaming Mouse' "Coordinate Transformation Matrix" 1 0 0 0 1 0 0 0 1
-xinput set-prop 'BenQ ZOWIE BenQ ZOWIE Gaming Mouse' "libinput Accel Speed" -0.25
-echo "Acceleration fixed for BenQ ZOWIE."
