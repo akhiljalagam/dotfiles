1cbbc663284fa814986280c7d7e83b597747b9a5
czarhex <czarhex@protonmail.com>
2022-07-31 02:02:56 +0200

    conker

--- .config/conky/start.sh
+++ .config/conky/start.sh
@@ -2,5 +2,8 @@
 
 pkill conky
 sleep 2s
-		
+
+# -------
+# change 'conky.conf' to 'conkydark.conf' for dark font and rings (nicer look on light wallpapers)
+# -------
 conky -d -c $HOME/.config/conky/conky.conf &> /dev/null &
